48b5e38e4aee13f0f41051139292a289bc496da8
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-03-31 18:43:03 -0600

    fix(vtkWSLinkClient): Export vtkWSLinkClient to properly initialized module

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@
 import './AvailableClasses';
 import components from './components';
 
+import SmartConnect from 'wslink/src/SmartConnect';
+import WSLinkClient from 'vtk.js/Sources/IO/Core/WSLinkClient';
+WSLinkClient.setSmartConnectClass(SmartConnect);
+
+export const vtkWSLinkClient = WSLinkClient;
+
 export default {
   install(Vue) {
     Object.keys(components).forEach((name) => {
