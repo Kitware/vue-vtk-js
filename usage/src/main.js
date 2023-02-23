930cf275b9ae3ed29c00238177eee0f084b98a97
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-02-23 14:00:10 -0700

    docs(usage): Update example to work with release

--- usage/src/main.js
+++ usage/src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import vtk from "vue-vtk-js";
-
-createApp(App).use(vtk).mount("#app");
+window.Vue.createApp({
+  template: `
+  <div style="margin: 0; padding: 0; width: 100%; height: 100vh;">
+    <vtk-view>
+      <vtk-geometry-representation>
+        <vtk-polydata
+          :points="[0, 0, 0, 0, 1, 0, 1, 0, 0]"
+          :polys="[3, 0, 1, 2]"
+        />
+      </vtk-geometry-representation>
+    </vtk-view>
+  </div>
+  `,
+})
+  .use(window.vue_vtk)
+  .mount("#app");
