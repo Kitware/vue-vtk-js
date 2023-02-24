d711a9bb36e378f71c16eee022a5dc15ca825b32
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-02-24 16:14:19 -0700

    fix(export): Export filter methods

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 // Ensure vtk.js classes available for Algorithm and Reader
 import "./AvailableClasses";
 import components from "./components";
+import filters from "./filters";
 
 export function install(Vue) {
   Object.keys(components).forEach((name) => {
     Vue.component(name, components[name]);
   });
+
+  if (window?.trame?.utils?.vtk) {
+    Object.keys(filters).forEach((name) => {
+      window.trame.utils.vtk[name] = filters[name];
+    });
+  }
 }
+
+export const vtkColorPresetNames = filters.vtkColorPresetNames;
+export const vtkLabel = filters.vtkLabel;
+export const ListToItem = filters.ListToItem;
+export const vtkColorPresetItems = filters.vtkColorPresetItems;
